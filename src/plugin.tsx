019f61f5a54a1b89612f971ae9cb763933e299cd
Yuri Sulyma <>
2022-01-31 04:12:12 -0500

    Export VideoRecorderPlugin

--- src/plugin.tsx
+++ src/plugin.tsx
@@ -1,14 +1,11 @@
 import "../style.css";
 
-export type {RecorderPlugin} from "./types";
-
-import RecordingControl from "./Control";
-export {RecordingControl};
-
-export {RecordingManager} from "./recording-manager";
-
+export {RecordingControl} from "./Control";
 export {Recorder} from "./recorder";
-export {ReplayDataRecorder} from "./recorders/replay-data-recorder";
 
 export {AudioRecorderPlugin} from "./recorders/audio-recorder";
 export {MarkerRecorderPlugin} from "./recorders/marker-recorder";
+export {VideoRecorderPlugin} from "./recorders/video-recorder";
+export {ReplayDataRecorder} from "./recorders/replay-data-recorder";
+export {RecordingManager} from "./recording-manager";
+export type {RecorderPlugin} from "./types";
